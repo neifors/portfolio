b041d6465d145089a7c01a3c8e45d963f7513784
isabel <isargp@gmail.com>
2022-06-06 19:50:54 +0100

    contact desabled cause google security settings changed

--- src/layout/Footer/index.js
+++ src/layout/Footer/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { ContactForm } from '../../components';
 import './index.css';
 
 
 export const Footer = () => {
    return(
       <div className='footer'>
+         {/* <ContactForm /> */}
          <p>© 2022 Isabel Repetto. Built using React ⚛️</p>
       </div>
    )
